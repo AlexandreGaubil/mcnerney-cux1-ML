84f6534e4fc7d5661a140ccc5ec18ae87defb5f3
Alexandre Gaubil <agaubil@gmail.com>
2022-01-04 14:12:17 -0500

    Update neg_ctrl_1000_rdm.py

--- code/neg_ctrl/neg_ctrl_1000_rdm.py
+++ code/neg_ctrl/neg_ctrl_1000_rdm.py
@@ -9,78 +9,25 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from tqdm import tqdm
-import os
+from itertools import chain
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import logistic_regression, fitting
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 # ---- MODEL ----
 
-# Construct the model using the same parameters as Weinreb (2020). Some
-# parameters had to be changed: solver, multi_class, & max_iter.
-lr = LogisticRegression(penalty = 'l2',
-                        dual = False,
-                        tol = 0.0001,
-                        # C = [0.008 in vitro; 0.004 in vivo]
-                        C = 0.008,
-                        fit_intercept = True,
-                        intercept_scaling = 1,
-                        class_weight = None,
-                        random_state = None,
-                        # solver = 'warn',
-                        solver = 'saga',
-                        # max_iter = 100,
-                        max_iter = 1000,
-                        # multi_class = 'warn',
-                        multi_class = 'multinomial',
-                        verbose = 0,
-                        warm_start = False,
-                        n_jobs = None)
-
-
-# ---- IMPORT DATA ----
+lr = logistic_regression.model
 
 data_matrix = np.loadtxt('./../../data/output/neg_control/rand_genes_neg_ctrl_data_all_cell_types.txt')
 feature_array = np.genfromtxt('./../../data/output/neg_control/rand_genes_neg_ctrl_feature_all_cell_types.txt',
                               dtype = 'str')
-
 df = pd.DataFrame(data_matrix)
 
-# One-hot encoding of feature_array
-lb = LabelBinarizer()
-lb.fit(feature_array)
-feature_one_hot = lb.transform(feature_array)
+accuracy_mtx = fitting.fit_model_rdm_gene_sets(df, feature_array, lr, 50, 10)
 
-# To recover labels from one hot encoding
-# lb.inverse_transform(feature_one_hot)
-
-print("Total number of genes from which to poll: {}".format(len(data_matrix[0])))
-
-
-# ---- MODEL & SCORING ----
-
-accuracy_array = []
-
-# Create 50 models of 1000 random genes each and calculate each model's accuracy
-for i in tqdm(range(1, 50)):
-    df_sample = df.sample(n = 1000,
-                          replace = False,
-                          axis = 1)
-
-    x_train, x_test, y_train, y_test = train_test_split(df_sample,
-                                                        feature_array,
-                                                        test_size=0.25,
-                                                        random_state=0)
-
-    # Fit the model to the data
-    model = lr.fit(x_train, y_train)
-
-    # Score the model accuracy
-    accuracy_array.append(model.score(x_test, y_test))
+accuracy_array = list(chain.from_iterable(accuracy_mtx))
 
 
 # ---- OUTPUT ----
@@ -89,18 +36,6 @@ print(accuracy_array)
 print("\nAverage accuracy: {}".format(np.mean(accuracy_array)))
 print("Standard deviation: {}".format(np.std(accuracy_array)))
 
-# Results for 3 cell types:
-# Average accuracy: 0.633
-# Standard deviation: 0.032
-
-# Results for 6 cell types with max_iter = 500 (11 failures to converge):
-# Average accuracy: 0.562
-# Standard deviation: 0.030
-
 # Results for 6 cell types with max_iter = 1,000:
-# Average accuracy: 0.566
-# Standard deviation: 0.030
-
-# Results for all cell types with max_iter = 1,000:
-# Average accuracy: 0.498
-# Standard deviation: 0.026
+# Average accuracy: 0.561
+# Standard deviation: 0.040
