90998d2ec72baac3e245b9fdda0cdb0b71a09903
Alexandre Gaubil <agaubil@gmail.com>
2022-01-04 14:43:19 -0500

    Update pos_ctrl_mouse_tf.py

--- code/pos_ctrl/pos_ctrl_mouse_tf.py
+++ code/pos_ctrl/pos_ctrl_mouse_tf.py
@@ -11,40 +11,16 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from tqdm import tqdm
-import os
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import logistic_regression, fitting
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 # ---- MODEL ----
 
-# Construct the model using the same parameters as Weinreb (2020). Some
-# parameters had to be changed: solver, multi_class, & max_iter.
-lr = LogisticRegression(penalty = 'l2',
-                        dual = False,
-                        tol = 0.0001,
-                        # C = [0.008 in vitro; 0.004 in vivo]
-                        C = 0.008,
-                        fit_intercept = True,
-                        intercept_scaling = 1,
-                        class_weight = None,
-                        random_state = None,
-                        # solver = 'warn',
-                        solver = 'saga',
-                        # max_iter = 100,
-                        max_iter = 1000,
-                        # multi_class = 'warn',
-                        multi_class = 'multinomial',
-                        verbose = 0,
-                        warm_start = False,
-                        n_jobs = None)
-
-
-# ---- IMPORT DATA ----
+lr = logistic_regression.model
 
 mouse_tf_df = pd.read_csv('./../../data/input/pos_ctrl/pos_ctrl_mus_musculus_TF_animal_TFDB.txt', sep = '\t')
 
@@ -57,30 +33,8 @@ feature_array = np.genfromtxt('./../../data/output/export_script/all_cells_all_g
 # Keep only genes that are mouse transcription factors
 df = data_matrix[data_matrix.columns[data_matrix.columns.isin(tfs)]]
 
-# One-hot encoding of feature_array
-lb = LabelBinarizer()
-lb.fit(feature_array)
-feature_one_hot = lb.transform(feature_array)
 
-# To recover labels from one hot encoding
-# lb.inverse_transform(feature_one_hot)
-
-
-# ---- MODEL & SCORING ----
-
-accuracy_array = []
-
-# Create 50 models and calculate each model's accuracy
-for i in tqdm(range(1, 50)):
-    x_train, x_test, y_train, y_test = train_test_split(df,
-                                                        feature_array,
-                                                        test_size=0.25)
-
-    # Fit the model to the data
-    model = lr.fit(x_train, y_train)
-
-    # Score the model accuracy
-    accuracy_array.append(model.score(x_test, y_test))
+accuracy_array = fitting.fit_model(df, feature_array, lr, 50)
 
 
 # ---- OUTPUT ----
@@ -90,5 +44,5 @@ print("\nAverage accuracy: {}".format(np.mean(accuracy_array)))
 print("Standard deviation: {}".format(np.std(accuracy_array)))
 
 # Results for all cell types:
-# Average accuracy: 0.618
-# Standard deviation: 0.022
+# Average accuracy: 0.619
+# Standard deviation: 0.028
