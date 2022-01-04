bc6faee07665e91e0a5037ef944c34965c1bdea3
Alexandre Gaubil <agaubil@gmail.com>
2022-01-04 11:19:11 -0500

    Update fitting.py

--- code/fitting.py
+++ code/fitting.py
@@ -7,22 +7,31 @@
 
 # ---- LIBRARIES ----
 
+import pandas
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 
 # ---- FITTING ----
 
-def fit_model(data_df, features_array, model, n_models):
+def fit_model_rdm_gene_sets(data_df, features_array, model, n_models, n_sets):
     accuracy_array = []
 
-    # Create 50 models of 1000 random genes each and calculate each model's accuracy
-    for i in tqdm(range(1, (n_models + 1))):
+    for _ in tqdm(range(1, (n_sets + 1))):
         df_sample = data_df.sample(n = 1000,
                                    replace = False,
                                    axis = 1)
 
-        x_train, x_test, y_train, y_test = train_test_split(df_sample,
+        accuracy_array.append(fit_model(df_sample, features_array, model, n_models))
+
+    return accuracy_array
+
+def fit_model(data_df, features_array, model, n_models):
+    accuracy_array = []
+
+    # Create 50 models of 1000 random genes each and calculate each model's accuracy
+    for _ in tqdm(range(1, (n_models + 1))):
+        x_train, x_test, y_train, y_test = train_test_split(data_df,
                                                             features_array,
                                                             test_size = 0.2)
 
