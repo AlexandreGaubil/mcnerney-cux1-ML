a306f0bebe9fa3ba962ced32f2b89fb1e1767298
Alexandre Gaubil <agaubil@gmail.com>
2022-01-04 22:11:19 -0500

    Added dnn to neg ctrl

--- code/neg_ctrl/neg_ctrl_1000_rdm.py
+++ code/neg_ctrl/neg_ctrl_1000_rdm.py
@@ -12,20 +12,21 @@ import pandas as pd
 from itertools import chain
 import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-import logistic_regression, fitting
+import logistic_regression, fitting, dnn
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # ---- MODEL ----
 
-lr = logistic_regression.model
+# model = logistic_regression.model
+model = dnn.model
 
 data_matrix = np.loadtxt('./../../data/output/neg_control/rand_genes_neg_ctrl_data_all_cell_types.txt')
 feature_array = np.genfromtxt('./../../data/output/neg_control/rand_genes_neg_ctrl_feature_all_cell_types.txt',
                               dtype = 'str')
 df = pd.DataFrame(data_matrix)
 
-accuracy_mtx = fitting.fit_model_rdm_gene_sets(df, feature_array, lr, 50, 10)
+accuracy_mtx = fitting.fit_model_rdm_gene_sets(df, feature_array, model, 50, 10)
 
 accuracy_array = list(chain.from_iterable(accuracy_mtx))
 
@@ -36,6 +37,11 @@ print(accuracy_array)
 print("\nAverage accuracy: {}".format(np.mean(accuracy_array)))
 print("Standard deviation: {}".format(np.std(accuracy_array)))
 
-# Results for 6 cell types with max_iter = 1,000:
+# Results for 6 cell types with logistic regression:
 # Average accuracy: 0.561
 # Standard deviation: 0.040
+
+# Results for 6 cell types with neural network:
+# Average accuracy: 0.529
+# Standard deviation: 0.036
+# Had 27 errors where lbfgs failed to converge
