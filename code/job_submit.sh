8335d9549721e17a5242a70fdb8eae4f3a1aeaf3
Alexandre Gaubil <agaubil@gmail.com>
2022-01-10 17:07:57 -0500

    Update job_submit.sh

--- code/job_submit.sh
+++ code/job_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=pos_ctrl_1000
-#SBATCH --output=pos_ctrl_1000_nn_and_lr.out
-#SBATCH --error=pos_ctrl_1000_nn_and_lr.err
+#SBATCH --job-name=pos_ctrl_prog
+#SBATCH --output=pos_ctrl_prog_nn_and_lr.out
+#SBATCH --error=pos_ctrl_prog_nn_and_lr.err
 #SBATCH --time=4:00:00
 #SBATCH --partition=broadwl
 #SBATCH --nodes=4
@@ -17,7 +17,9 @@ module load python
 # python neg_ctrl/neg_ctrl_1000_rdm.py
 
 # POS CTRL
-python pos_ctrl/pos_ctrl_1000_rdm_most_var.py
+# python pos_ctrl/pos_ctrl_1000_rdm_most_var.py
+# python pos_ctrl/pos_ctrl_mouse_tf.p
+python pos_ctrl/pos_ctrl_prog.py
 
 # HYPER-PARAM TUNING DNN
 # python cux1/cux1.py
