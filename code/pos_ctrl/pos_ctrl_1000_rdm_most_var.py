86463dced3e4a658b230c6194d650f5bb0c31170
Alexandre Gaubil <agaubil@gmail.com>
2022-01-10 14:12:59 -0500

    Updated py scripts to change files

--- code/pos_ctrl/pos_ctrl_1000_rdm_most_var.py
+++ code/pos_ctrl/pos_ctrl_1000_rdm_most_var.py
@@ -20,11 +20,11 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # ---- MODEL ----
 
-print('POSITIVE CONTROL 1,000 GENES OUT OF 2,000 MOST VARIABLE GENES')
+print('POSITIVE CONTROL: 1,000 GENES OUT OF 2,000 MOST VARIABLE GENES\n\n\n')
 
 for model in [models.log_reg, models.neural_network]:
-    data_matrix = np.loadtxt('./../../data/output/pos_control/var_2000_pos_ctrl_data.txt')
-    feature_array = np.genfromtxt('./../../data/output/pos_control/var_2000_pos_ctrl_feature.txt',
+    data_matrix = np.loadtxt('./../../data/input/pos_control/pos_ctrl_var_2000_pos_ctrl_data.txt')
+    feature_array = np.genfromtxt('./../../data/input/pos_control/pos_ctrl_var_2000_pos_ctrl_feature.txt',
                                 dtype = 'str')
     df = pd.DataFrame(data_matrix)
 
@@ -34,9 +34,9 @@ for model in [models.log_reg, models.neural_network]:
 
     # ---- OUTPUT ----
 
-    print('./../../data/output/pos_control/var_2000_pos_ctrl_data.txt')
+    print('./../../data/input/pos_control/pos_ctrl_var_2000_pos_ctrl_data.txt')
     print(model)
-    print("\nAverage accuracy: {}".format(np.mean(accuracy_array)))
+    print("Average accuracy: {}".format(np.mean(accuracy_array)))
     print("Standard deviation: {}".format(np.std(accuracy_array)))
     print('\n\n')
 
