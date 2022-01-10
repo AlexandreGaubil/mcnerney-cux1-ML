e8441dfab737a3760883ed9f1b53a2a7862c81ef
Alexandre Gaubil <gaubil@uchicago.edu>
2022-01-10 10:25:40 -0600

    About to run neg_ctrl_nn

--- code/job_submit.sh
+++ code/job_submit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=cux1
-#SBATCH --output=cux1.out
-#SBATCH --error=cux1.err
-#SBATCH --time=24:00:00
+#SBATCH --output=neg_ctrl_nn.out
+#SBATCH --error=neg_ctrl_nn.err
+#SBATCH --time=8:00:00
 #SBATCH --partition=broadwl
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=14
 #SBATCH --mem-per-cpu=200
 #SBATCH --mail-user=gaubil@uchicago.edu
 #SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL0
+#SBATCH --mail-type=FAIL
 
 module load python
 
