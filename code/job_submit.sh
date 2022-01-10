88e8ddbf144c0d925e1aae121db60e3a87935a33
Alexandre Gaubil <agaubil@gmail.com>
2022-01-10 11:20:35 -0500

    Update job_submit.sh

--- code/job_submit.sh
+++ code/job_submit.sh
@@ -6,12 +6,15 @@
 #SBATCH --partition=broadwl
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=14
-#SBATCH --mem-per-cpu=2000
+#SBATCH --mem-per-cpu=200
+#SBATCH --mail-user=gaubil@uchicago.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL0
 
 module load python
 
 # NEG CTRL
-# python neg_ctrl/neg_ctrl_1000_rdm.py
+python neg_ctrl/neg_ctrl_1000_rdm.py
 
 # HYPER-PARAM TUNING DNN
 python cux1/cux1.py
