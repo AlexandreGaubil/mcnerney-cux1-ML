e9eac33dae2a5ad5177df2fe379275247ad29aa4
Alexandre Gaubil <agaubil@gmail.com>
2022-01-24 10:23:25 -0600

    Prog pos ctrl 3 cells

--- code/pos_ctrl/pos_ctrl_prog.py
+++ code/pos_ctrl/pos_ctrl_prog.py
@@ -20,9 +20,11 @@ import models, fitting
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 print('POS CTRL PROGENITOR CELL\n\n\n')
+print('./../../data/output/export_script/three_cells_all_genes_data_headers.txt')
 
 # ---- IMPORT DATA ----
-for model in [models.log_reg, models.neural_network]:
+#for model in [models.log_reg, models.neural_network]:
+for model in [models.log_reg]:
     input_dir_str = './../../data/input/pos_ctrl/pos_ctrl_progenitor_signature_genes/'
     input_dir = os.fsencode(input_dir_str)
 
@@ -34,8 +36,11 @@ for model in [models.log_reg, models.neural_network]:
             sign_gene_df = pd.read_csv(os.path.join(input_dir_str, filename))
             sign_genes = sign_gene_df['Symbol'].tolist()
 
-            data_matrix = pd.read_csv('./../../data/output/export_script/all_cells_all_genes_data_headers.txt', sep = ' ')
-            feature_array = np.genfromtxt('./../../data/output/export_script/all_cells_all_genes_features.txt', dtype = 'str')
+            data_matrix = pd.read_csv('./../../data/output/export_script/three_cells_all_genes_data_headers.txt', sep = ' ')
+            feature_array = np.genfromtxt('./../../data/output/export_script/three_cells_all_genes_features.txt', dtype = 'str')
+
+            # data_matrix = pd.read_csv('./../../data/output/export_script/all_cells_all_genes_data_headers.txt', sep = ' ')
+            # feature_array = np.genfromtxt('./../../data/output/export_script/all_cells_all_genes_features.txt', dtype = 'str')
 
             # Keep only genes that are signature genes
             df = data_matrix[data_matrix.columns[data_matrix.columns.isin(sign_genes)]]
