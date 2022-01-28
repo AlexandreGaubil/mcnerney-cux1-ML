abc4c829d51fbfac83a144192f4b2d95e2212c6e
Alexandre Gaubil <agaubil@gmail.com>
2022-01-28 00:21:12 -0600

    Restructured DEGs & Cux1

--- code/cux1/DEGs.py
+++ code/cux1/DEGs.py
@@ -5,45 +5,36 @@
 # dataset for our models (both linear regression & dnn).
 #
 
+
 import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from fitting_filter_genes import fit_model_filter_genes
-import models
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import models
+import run_model as run
 
-# models_list = [models.log_reg, models.neural_network, models.hyper_param]
-# n_models_list = [50, 50, 1]
-# hyper_param_tuning_list = [False, False, True]
-models_list = [models.log_reg]
-n_models_list = [50]
-hyper_param_tuning_list = [False]
 
-gene_files = [
+goi_file_sets = [
     ['./../../data/input/gene_sets/wl001_shRNA_LowRen_DEGs.txt'],
-    ['./../../data/input/gene_sets/Molly_shCux1_bulk_RNA_DEGs.txt']
-]
+    ['./../../data/input/gene_sets/Molly_shCux1_bulk_RNA_DEGs.txt']]
+models = [models.log_reg, models.hyper_param]
+data_mtx_files = [
+    './../../data/output/export_script/three_cells_all_genes_data_headers.txt',
+    './../../data/output/export_script/all_cells_all_genes_data_headers.txt']
+data_mtx_col_seps = [' ', ' ']
+features_array_files = [
+    './../../data/output/export_script/three_cells_all_genes_features.txt',
+    './../../data/output/export_script/all_cells_all_genes_features.txt']
+n_models = [50, 1]
+hyper_param_tuning = [False, True]
 
-data_mtr_files = [
-    './../../data/output/export_script/all_cells_all_genes_data_headers.txt',
-    './../../data/output/export_script/three_cells_all_genes_data_headers.txt'
-]
 
-features_files = [
-    './../../data/output/export_script/all_cells_all_genes_features.txt',
-    './../../data/output/export_script/three_cells_all_genes_features.txt'
-]
-
-for i in range(len(models_list)):
-    for j in range(len(gene_files)):
-        for k in range(len(data_mtr_files)):
-            print(models_list[i])
-            print(gene_files[j])
-            print(data_mtr_files[k])
-            fit_model_filter_genes(model = models_list[i],
-                                genes_of_interest_files = gene_files[j],
-                                data_matrix_file = data_mtr_files[k],
-                                data_matrix_column_sep = ' ',
-                                features_array_file = features_files[k],
-                                n_models = n_models_list[i],
-                                hyper_param_tuning = hyper_param_tuning_list[i])
+for i in range(len(goi_file_sets)):
+    print("\n\n\nCUX1 RELATED GENES DEGS")
+    run.run_model(
+        models,
+        goi_file_sets[i],
+        data_mtx_files,
+        data_mtx_col_seps,
+        features_array_files,
+        n_models,
+        hyper_param_tuning)
