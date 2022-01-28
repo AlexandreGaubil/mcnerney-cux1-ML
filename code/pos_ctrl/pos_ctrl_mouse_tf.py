407f248060a04845875fbb1ccad4223e473bccd2
Alexandre Gaubil <agaubil@gmail.com>
2022-01-27 23:57:11 -0600

    Restructured mouse tfs

--- code/pos_ctrl/pos_ctrl_mouse_tf.py
+++ code/pos_ctrl/pos_ctrl_mouse_tf.py
@@ -7,39 +7,33 @@
 # Weinreb (2020)
 #
 
-# ---- LIBRARIES ----
 
-import numpy as np
-import pandas as pd
 import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-import models, fitting
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import models
+import run_model as run
 
-print('POSITIVE CONTROL MOUSE TFS\n\n\n')
 
-# ---- MODEL ----
-for model in [models.log_reg, models.neural_network]:
-    mouse_tf_df = pd.read_csv('./../../data/input/pos_ctrl/pos_ctrl_mus_musculus_TF_animal_TFDB.txt', sep = '\t')
+models = [models.log_reg, models.hyper_param]
+goi_files = ['./../../data/input/gene_sets/mouse_tfs.txt']
+data_mtx_files = [
+    './../../data/output/export_script/three_cells_all_genes_data_headers.txt',
+    './../../data/output/export_script/all_cells_all_genes_data_headers.txt']
+data_mtx_col_seps = [' ', ' ']
+features_array_files = [
+    './../../data/output/export_script/three_cells_all_genes_features.txt',
+    './../../data/output/export_script/all_cells_all_genes_features.txt']
+n_models = [50, 1]
+hyper_param_tuning = [False, True]
 
-    # List of transcriptor factors
-    tfs = mouse_tf_df['Symbol'].tolist()
 
-    data_matrix = pd.read_csv('./../../data/output/export_script/three_cells_all_genes_data_headers.txt', sep = ' ')
-    feature_array = np.genfromtxt('./../../data/output/export_script/three_cells_all_genes_features.txt', dtype = 'str')
-
-    #data_matrix = pd.read_csv('./../../data/output/export_script/all_cells_all_genes_data_headers.txt', sep = ' ')
-    #feature_array = np.genfromtxt('./../../data/output/export_script/all_cells_all_genes_features.txt', dtype = 'str')
-
-    # Keep only genes that are mouse transcription factors
-    df = data_matrix[data_matrix.columns[data_matrix.columns.isin(tfs)]]
-
-    accuracy_array = fitting.fit_model(df, feature_array, model, 50)
-
-    # ---- OUTPUT ----
-    print('./../../data/output/export_script/three_cells_all_genes_data_headers.txt')
-    print(model)
-    print("Average accuracy: {}".format(np.mean(accuracy_array)))
-    print("Standard deviation: {}".format(np.std(accuracy_array)))
-    print('\n\n')
+print("\n\n\POSITIVE CONTROL: MOUSE TFS")
+run.run_model(
+    models,
+    goi_files,
+    data_mtx_files,
+    data_mtx_col_seps,
+    features_array_files,
+    n_models,
+    hyper_param_tuning)
