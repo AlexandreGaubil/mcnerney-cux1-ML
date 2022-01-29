c910bbb0eb239fff8fd382c95bdd47a626d00dac
Alexandre Gaubil <agaubil@gmail.com>
2022-01-28 23:12:12 -0600

    Update graph_results.R

--- results/graph_results.R
+++ results/graph_results.R
@@ -6,6 +6,7 @@
 
 library(ggplot2)
 library(dplyr)
+library(stats)
 
 # String Variables
 analysis.neg_ctrl = 'Negative Control - 1,000 Random Genes'
@@ -18,6 +19,9 @@ model.log_reg = 'Logistic  Regression'
 model.nn = 'Neural Network'
 dataset.in_vivo = 'In vivo'
 dataset.in_vitro = 'In vitro'
+n.bootstraps.default = 50
+n.rdm_gene_samples.default.yes = 10
+n.rdm_gene_samples.default.no = 1
 
 
 df <- data.frame(analysis = character(),
@@ -25,17 +29,10 @@ df <- data.frame(analysis = character(),
                  n_cell_types = integer(),
                  model = character(),
                  n_genes = integer(),
+                 n_boostraps = integer(),
+                 n_rdm_gene_samples = integer(),
                  average = numeric(),
                  std_deviation = numeric())
-
-                 #Log_Reg_All_Cells.avg = numeric(),
-                 #Log_Reg_All_Cells.std = numeric(),
-                 #Log_Reg_Three_Cells.avg = numeric(),
-                 #Log_Reg_Three_Cells.std = numeric(),
-                 #NN_All_Cells.avg = numeric(),
-                 #NN_All_Cells.std = numeric(),
-                 #NN_Three_cells.avg = numeric(),
-                 #NN_Three_cells.std = numeric())
 
 # Add Data
 
@@ -46,6 +43,8 @@ df <- add_row(df,
               n_cell_types = 6,
               model = model.log_reg,
               n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
               average = 0.555592841163311,
               std_deviation = 0.03279236876430187)
 
@@ -55,6 +54,8 @@ df <- add_row(df,
               n_cell_types = 3,
               model = model.log_reg,
               n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
               average = 0.6313383838383839,
               std_deviation = 0.03139193610927017)
 
@@ -64,8 +65,10 @@ df <- add_row(df,
               n_cell_types = 6,
               model = model.nn,
               n_genes = 1000,
-              average = 0.5648769574944071,
-              std_deviation = 0.0422694488315239)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.5681953765846384,
+              std_deviation = 0.03931263003915425)
 
 df <- add_row(df,
               analysis = analysis.neg_ctrl,
@@ -73,82 +76,239 @@ df <- add_row(df,
               n_cell_types = 3,
               model = model.nn,
               n_genes = 1000,
-              average = 0.6157407407407407,
-              std_deviation = 0.03302732544918089)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.6168181818181819,
+              std_deviation = 0.03663378189765403)
 
 
-
-
-
-
-df <- add_row(df, 
-              analysis = "Negative Control - 1,000 Random Genes",
+## Positive Control: 1,000 Most Variable Genes
+df <- add_row(df,
+              analysis = analysis.most_var,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.log_reg,
               n_genes = 1000,
-              Log_Reg_All_Cells.avg = 0.555592841163311,
-              Log_Reg_All_Cells.std = 0.03279236876430187,
-              Log_Reg_Three_Cells.avg = 0.6313383838383839,
-              Log_Reg_Three_Cells.std = 0.03139193610927017,
-              NN_All_Cells.avg = 0.5648769574944071,
-              NN_All_Cells.std = 0.0422694488315239,
-              NN_Three_cells.avg = 0.6157407407407407,
-              NN_Three_cells.std = 0.03302732544918089)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.7112900820283371,
+              std_deviation = 0.025929285583589912)
 
 df <- add_row(df,
-              analysis = "Positive Control - 1,000 Most Variable Genes",
+              analysis = analysis.most_var,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.log_reg,
               n_genes = 1000,
-              Log_Reg_All_Cells.avg = 0.7112900820283371,
-              Log_Reg_All_Cells.std = 0.025929285583589912,
-              Log_Reg_Three_Cells.avg = 0.7496885521885522,
-              Log_Reg_Three_Cells.std = 0.023968596188772454,
-              NN_All_Cells.avg = NaN,
-              NN_All_Cells.std = NaN,
-              NN_Three_cells.avg = NaN,
-              NN_Three_cells.std = NaN)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.7496885521885522,
+              std_deviation = 0.023968596188772454)
 
 df <- add_row(df,
-              analysis = "Positive Control - Mouse Transcriptor Factors",
+              analysis = analysis.most_var,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.nn,
               n_genes = 1000,
-              Log_Reg_All_Cells.avg = 0.601290082028337,
-              Log_Reg_All_Cells.std = 0.029800005978490363,
-              Log_Reg_Three_Cells.avg = 0.6621212121212122,
-              Log_Reg_Three_Cells.std = 0.032926233048637475,
-              NN_All_Cells.avg = 0.5779269202087994,
-              NN_All_Cells.std = 0.041152907503110715,
-              NN_Three_cells.avg = 0.6510942760942762,
-              NN_Three_cells.std = 0.035732298445776293)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = NaN,
+              std_deviation = NaN)
 
 df <- add_row(df,
-              analysis = "Experimental - CUX1 Binding Jeff",
+              analysis = analysis.most_var,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.nn,
               n_genes = 1000,
-              Log_Reg_All_Cells.avg = 0.5919313944817302,
-              Log_Reg_All_Cells.std = 0.0311322241168891,
-              Log_Reg_Three_Cells.avg = 0.6548484848484849,
-              Log_Reg_Three_Cells.std = 0.030048128248251076,
-              NN_All_Cells.avg = 0.592841163310962,
-              NN_All_Cells.std = 0.02163208009598849,
-              NN_Three_cells.avg = 0.6426767676767676,
-              NN_Three_cells.std = 0.045454545454545456)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = NaN,
+              std_deviation = NaN)
 
-df <- add_row(df, 
-              analysis = "Experimental - CUX1 Binding Jeff & Molly",
+
+## Positive Control: Mouse Transcription Factors
+df <- add_row(df,
+              analysis = analysis.mouse_tf,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.log_reg,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.601290082028337,
+              std_deviation = 0.029800005978490363)
+
+df <- add_row(df,
+              analysis = analysis.mouse_tf,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.log_reg,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.6621212121212122,
+              std_deviation = 0.032926233048637475)
+
+df <- add_row(df,
+              analysis = analysis.mouse_tf,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.nn,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.5779269202087994,
+              std_deviation = 0.041152907503110715)
+
+df <- add_row(df,
+              analysis = analysis.mouse_tf,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.nn,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.6510942760942762,
+              std_deviation = 0.035732298445776293)
+
+
+## Experimental: CUX1 Binding Targets Jeff
+df <- add_row(df,
+              analysis = analysis.exp_jeff,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.log_reg,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.5919313944817302,
+              std_deviation = 0.0311322241168891)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.log_reg,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.6548484848484849,
+              std_deviation = 0.030048128248251076)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.nn,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.592841163310962,
+              std_deviation = 0.02163208009598849)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.nn,
+              n_genes = 1000,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.yes,
+              average = 0.6426767676767676,
+              std_deviation = 0.045454545454545456)
+
+
+
+
+## Experimental: CUX1 Binding Targets Jeff inter Molly
+df <- add_row(df,
+              analysis = analysis.exp_jeff_molly,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.log_reg,
               n_genes = 891,
-              Log_Reg_All_Cells.avg = 0.6622147651006712,
-              Log_Reg_All_Cells.std = 0.026800381316671153,
-              Log_Reg_Three_Cells.avg = 0.719621212121212,
-              Log_Reg_Three_Cells.std = 0.02390256564948832,
-              NN_All_Cells.avg = 0.6308724832214765,
-              NN_All_Cells.std = 0,
-              NN_Three_cells.avg = 0.6856060606060606,
-              NN_Three_cells.std = 0)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.6622147651006712,
+              std_deviation = 0.026800381316671153)
 
-df <- add_row(df, 
-              analysis = "Experimental - CUX1 Binding Jeff & Weihan",
+df <- add_row(df,
+              analysis = analysis.exp_jeff_molly,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.log_reg,
+              n_genes = 891,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.719621212121212,
+              std_deviation = 0.02390256564948832)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff_molly,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.nn,
+              n_genes = 891,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.6308724832214765,
+              std_deviation = NaN)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff_molly,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.nn,
+              n_genes = 891,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.6856060606060606,
+              std_deviation = NaN)
+
+
+## Experimental: CUX1 Binding Targets Jeff inter Weihan
+df <- add_row(df,
+              analysis = analysis.exp_jeff_weihan,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.log_reg,
               n_genes = 923,
-              Log_Reg_All_Cells.avg = 0.6821476510067113,
-              Log_Reg_All_Cells.std = 0.020134675610241227,
-              Log_Reg_Three_Cells.avg = 0.7253030303030303,
-              Log_Reg_Three_Cells.std = 0.025971040593225157,
-              NN_All_Cells.avg = 0.6778523489932886,
-              NN_All_Cells.std = 0,
-              NN_Three_cells.avg = 0.7159090909090909,
-              NN_Three_cells.std = 0)
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.6821476510067113,
+              std_deviation = 0.020134675610241227)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff_weihan,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.log_reg,
+              n_genes = 923,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.7253030303030303,
+              std_deviation = 0.025971040593225157)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff_weihan,
+              dataset = dataset.in_vitro,
+              n_cell_types = 6,
+              model = model.nn,
+              n_genes = 923,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.6778523489932886,
+              std_deviation = NaN)
+
+df <- add_row(df,
+              analysis = analysis.exp_jeff_weihan,
+              dataset = dataset.in_vitro,
+              n_cell_types = 3,
+              model = model.nn,
+              n_genes = 923,
+              n_boostraps = n.bootstraps.default,
+              n_rdm_gene_samples = n.rdm_gene_samples.default.no,
+              average = 0.7159090909090909,
+              std_deviation = NaN)
