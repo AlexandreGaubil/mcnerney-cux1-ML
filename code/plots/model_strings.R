7ea11f1277d55b8c0a63136b1cd42fc28ef575fb
Alexandre Gaubil <agaubil@gmail.com>
2022-02-04 18:32:56 -0600

    Moved files

---
+++ code/plots/model_strings.R
@@ -0,0 +1,16 @@
+# String Variables
+analysis.neg_ctrl = 'Negative Control - 1,000 Random Genes'
+analysis.most_var = 'Positive Control - 1,000 Most Variable Genes'
+analysis.mouse_tf = 'Positive Control - Mouse Transcriptor Factors'
+analysis.exp_jeff = 'Experimental - CUX1 Binding Targets Jeff'
+analysis.exp_jeff_molly = 'Experimental - CUX1 Binding Targets Jeff & Molly'
+analysis.exp_jeff_weihan = 'Experimental - CUX1 Binding Targets Jeff & Weihan'
+analysis.exp_molly_deg = 'Experimental - CUX1 Molly DEGs'
+analysis.exp_weihan_deg = 'Experimental - CUX1 Weihan DEGs'
+model.log_reg = 'Logistic Regression'
+model.nn = 'Neural Network'
+dataset.in_vivo = 'In vivo'
+dataset.in_vitro = 'In vitro'
+n.bootstraps.default = 50
+n.rdm_gene_samples.default.yes = 10
+n.rdm_gene_samples.default.no = 1
