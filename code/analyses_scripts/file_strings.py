3d01ce3a5ab0dd762a6677607666b4f6330f0be7
Alexandre Gaubil <agaubil@gmail.com>
2022-04-08 11:01:48 -0500

    Set files to in vitro again for human genes

--- code/analyses_scripts/file_strings.py
+++ code/analyses_scripts/file_strings.py
@@ -1,12 +1,12 @@
 # THREE_CELLS_ALL_GENES_DATA = './../../../data/in_vitro/analyses_input/three_cells_all_genes_data_headers.txt'
 # THREE_CELLS_ALL_GENES_FEATURES = './../../../data/in_vitro/analyses_input/three_cells_all_genes_features.txt'
-# ALL_CELLS_ALL_GENES_DATA = './../../../data/in_vitro/analyses_input/all_cells_all_genes_data_headers.txt'
-# ALL_CELLS_ALL_GENES_FEATURES = './../../../data/in_vitro/analyses_input/all_cells_all_genes_features.txt'
+ALL_CELLS_ALL_GENES_DATA = './../../../data/in_vitro/analyses_input/all_cells_all_genes_data_headers.txt'
+ALL_CELLS_ALL_GENES_FEATURES = './../../../data/in_vitro/analyses_input/all_cells_all_genes_features.txt'
 
 # THREE_CELLS_ALL_GENES_DATA = './../../../data/in_vivo/analyses_input/three_cells_all_genes_data_headers.txt'
 # THREE_CELLS_ALL_GENES_FEATURES = './../../../data/in_vivo/analyses_input/three_cells_all_genes_features.txt'
-ALL_CELLS_ALL_GENES_DATA = './../../../data/in_vivo/analyses_input/all_cells_all_genes_data_headers.txt'
-ALL_CELLS_ALL_GENES_FEATURES = './../../../data/in_vivo/analyses_input/all_cells_all_genes_features.txt'
+# ALL_CELLS_ALL_GENES_DATA = './../../../data/in_vivo/analyses_input/all_cells_all_genes_data_headers.txt'
+# ALL_CELLS_ALL_GENES_FEATURES = './../../../data/in_vivo/analyses_input/all_cells_all_genes_features.txt'
 
 GOI_ALL_GENES = './../../../data/gene_sets/all_genes.txt'
 
